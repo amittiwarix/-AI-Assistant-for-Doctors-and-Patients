717703253cb31d5ce1f92bc3404ae7e7d622e99e
Amit Tiwari <amittiwarix@gmail.com>
2026-03-21 17:21:15 +0530

    requirements installed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ai_assistant",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    description="A simple AI assistant",
+    author="Amit Tiwari",
+    author_email="amittiwarix@gmail.com",
+)
